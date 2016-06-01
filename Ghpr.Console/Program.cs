aded2ce1b4ae750c88000cc506a3ddacb6bfd2b3
Evgeniy Kosjakov <42_@mail.ru>
2016-06-01 12:05:45 +0300

    console, settings

---
+++ Ghpr.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Ghpr.Core;
+
+namespace Ghpr.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var res = "n";
+            while (!res.Equals("y"))
+            {
+                Reporter.ExtractReportFiles();
+                System.Console.WriteLine("Exit? (y/n)");
+                res = System.Console.ReadLine() ?? "n";
+            }
+        }
+    }
+}
--- Ghpr.Core/Reporter.cs
+++ Ghpr.Core/Reporter.cs
@@ -1,12 +1,19 @@
-﻿using System.Configuration;
+﻿using System.IO;
+using Ghpr.Core.EmbeddedResources;
+using Ghpr.Core.Enums;
 
 namespace Ghpr.Core
 {
     public static class Reporter
     {
-        public static string OutputPath => ConfigurationManager.AppSettings["outputPath"];
+        public static string OutputPath => Properties.Settings.Default.outputPath;
+        public const string Src = "src";
 
-
+        public static void ExtractReportFiles()
+        {
+            var re = new ResourceExtractor(Path.Combine(OutputPath, Src));
+            re.Extract(Resource.All);
+        }
 
     }
 }
