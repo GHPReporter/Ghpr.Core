9f9046ea6b13d532465c6b39695eeb523dde8b47
Evgeniy Kosjakov <42_@mail.ru>
2016-06-07 18:38:17 +0300

    interfaces and colors

---
+++ Ghpr.Core/Interfaces/ITestRun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ghpr.Core.Interfaces
+{
+    public interface ITestRun
+    {
+        string Name { get; set; }
+        string FullName { get; set; }
+        double TestDuration { get; set; }
+        DateTime DateTimeStart { get; set; }
+        DateTime DateTimeFinish { get; set; }
+        string TestStackTrace { get; set; }
+        string TestMessage { get; set; }
+        string Result { get; set; }
+        Guid Guid { get; set; }
+        List<ITestScreenshot> Screenshots { get; set; }
+        List<ITestEvent> Events { get; set; }
+    }
+}
