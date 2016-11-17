3c3e0330376aa88c297d7555f03461117687f6c0
Evgeniy Kosjakov <42_@mail.ru>
2016-11-17 22:10:04 +0300

    Taker refactoring

--- Ghpr.Core/Utils/Taker.cs
+++ Ghpr.Core/Utils/Taker.cs
@@ -8,61 +8,58 @@ namespace Ghpr.Core.Utils
 {
     internal static class Taker
     {
-        public static string GetScreenName(DateTime now, ImageFormat format = null)
+        public static void TakeScreenshot(string screenPath, ImageFormat format, DateTime creationTime = default(DateTime))
+        {
+            var b = Screen.PrimaryScreen.Bounds;
+            using (var btm = new Bitmap(b.Width, b.Height))
+            {
+                using (var g = Graphics.FromImage(btm))
+                {
+                    g.CopyFromScreen(b.X, b.Y, 0, 0, btm.Size, CopyPixelOperation.SourceCopy);
+
+                    SaveScreenshot(screenPath, btm, format, creationTime);
+                }
+            }
+        }
+
+        public static string GetScreenName(DateTime now, ImageFormat format)
         {
             format = format ?? ImageFormat.Png;
             return $"img_{now.ToString("yyyyMMdd_HHmmssfff")}.{format.ToString().ToLower()}";
         }
 
-        public static string TakeScreenshot(string screenPath, DateTime creationTime = default(DateTime))
+        public static string SaveScreenshot(string screenPath, byte[] screenBytes,
+            ImageFormat format, DateTime creationTime = default(DateTime))
         {
-            if (!Directory.Exists(screenPath))
+            using (var image = Image.FromStream(new MemoryStream(screenBytes)))
             {
-                Directory.CreateDirectory(screenPath);
+                return SaveScreenshot(screenPath, new Bitmap(image), format, creationTime);
             }
-            var format = ImageFormat.Png;
-            var now = DateTime.Now;
-            creationTime = creationTime.Equals(default(DateTime)) ? now : creationTime;
-            var screenName = GetScreenName(creationTime, format);
-
-            using (var bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
-                                            Screen.PrimaryScreen.Bounds.Height))
-            {
-                using (var g = Graphics.FromImage(bmpScreenCapture))
-                {
-                    g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
-                                     Screen.PrimaryScreen.Bounds.Y,
-                                     0, 0,
-                                     bmpScreenCapture.Size,
-                                     CopyPixelOperation.SourceCopy);
-
-                    var file = Path.Combine(screenPath, screenName);
-                    bmpScreenCapture.Save(file, format);
-                    var fileInfo = new FileInfo(file);
-                    fileInfo.Refresh();
-                    fileInfo.CreationTime = creationTime;
-
-                }
-            }
-            return screenName;
         }
 
-        public static string TakeScreenshot(string screenPath, Bitmap screen, DateTime creationTime = default(DateTime))
+        public static string SaveScreenshot(string screenPath, Image img,
+            ImageFormat format, DateTime creationTime = default(DateTime))
+        {
+            return SaveScreenshot(screenPath, new Bitmap(img), format, creationTime);
+        }
+
+        public static string SaveScreenshot(string screenPath, Bitmap screen,
+            ImageFormat format, DateTime creationTime = default(DateTime))
         {
             if (!Directory.Exists(screenPath))
             {
                 Directory.CreateDirectory(screenPath);
             }
-            var format = ImageFormat.Png;
-            var now = DateTime.Now;
-            creationTime = creationTime.Equals(default(DateTime)) ? now : creationTime;
+            creationTime = creationTime.Equals(default(DateTime)) ? DateTime.Now : creationTime;
             var screenName = GetScreenName(creationTime, format);
             var file = Path.Combine(screenPath, screenName);
+
             screen.Save(file, format);
+
             var fileInfo = new FileInfo(file);
             fileInfo.Refresh();
             fileInfo.CreationTime = creationTime;
-            
+
             return screenName;
         }
     }
