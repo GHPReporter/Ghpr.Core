0427234852443090d735f1144e390684ffc14a59
Evgeniy Kosjakov <42_@mail.ru>
2017-01-11 19:58:16 +0300

    update

--- Ghpr.Core/Properties/AssemblyInfo.cs
+++ Ghpr.Core/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Evgeniy Kosjakov")]
 [assembly: AssemblyProduct("Ghpr.Core")]
-[assembly: AssemblyCopyright("Copyright © Evgeniy Kosjakov 2016")]
+[assembly: AssemblyCopyright("Copyright © Evgeniy Kosjakov 2016-2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
