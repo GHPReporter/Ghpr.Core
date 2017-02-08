30fb50c70138022d981e28165f1a796332d68d3c
Evgeniy Kosjakov <42_@mail.ru>
2017-02-08 18:44:36 +0300

    fix

--- Ghpr.Core/Helpers/ItemInfoHelper.cs
+++ Ghpr.Core/Helpers/ItemInfoHelper.cs
@@ -12,15 +12,13 @@ namespace Ghpr.Core.Helpers
     {
         public static void SaveItemInfo(string path, string filename, ItemInfo itemInfo, bool removeExisting = true)
         {
+            var ii = new ItemInfo(itemInfo);
             var serializer = new JsonSerializer();
             Paths.Create(path);
             var fullItemInfoPath = Path.Combine(path, filename);
             if (!File.Exists(fullItemInfoPath))
             {
-                var items = new List<ItemInfo>
-                {
-                    itemInfo
-                };
+                var items = new List<ItemInfo>(1) {ii};
                 using (var file = File.CreateText(fullItemInfoPath))
                 {
                     serializer.Serialize(file, items);
@@ -36,13 +34,13 @@ namespace Ghpr.Core.Helpers
                 var itemsToSave = new List<ItemInfo>(existingItems.Count);
                 existingItems.ForEach(i => { itemsToSave.Add(new ItemInfo(i)); });
 
-                if (removeExisting && itemsToSave.Any(i => i.Guid.Equals(itemInfo.Guid)))
+                if (removeExisting && itemsToSave.Any(i => i.Guid.Equals(ii.Guid)))
                 {
-                    itemsToSave.RemoveAll(i => i.Guid.Equals(itemInfo.Guid));
+                    itemsToSave.RemoveAll(i => i.Guid.Equals(ii.Guid));
                 }
-                if (!itemsToSave.Contains(itemInfo, new ItemInfoComparer()))
+                if (!itemsToSave.Contains(ii, new ItemInfoComparer()))
                 {
-                    itemsToSave.Add(itemInfo);
+                    itemsToSave.Add(new ItemInfo(ii));
                 }
                 using (var file = File.CreateText(fullItemInfoPath))
                 {
