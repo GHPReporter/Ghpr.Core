e4e6138a60bc484ef8a4c661205db02e94697ef3
Evgeniy Kosjakov <42_@mail.ru>
2017-04-19 16:46:51 +0300

    log refactor

--- Ghpr.Core/Utils/Log.cs
+++ Ghpr.Core/Utils/Log.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.IO;
+using Ghpr.Core.Helpers;
 using Ghpr.Core.Interfaces;
 
 namespace Ghpr.Core.Utils
 {
     public class Log : ILog
     {
+        private string _logFile;
+        private readonly string _output;
+        private readonly ActionHelper _actionHelper;
+
         public Log(string outputPath, string logFile = "")
         {
             _output = outputPath;
             _logFile = logFile.Equals("") ? "GHPReporter.txt" : logFile;
+            _actionHelper = new ActionHelper(outputPath);
         }
-
-        private string _logFile;
-        private readonly string _output;
-
+        
         public void WriteToFile(string msg, string fileName)
         {
             Paths.Create(_output);
@@ -34,23 +37,27 @@ namespace Ghpr.Core.Utils
 
         public void Write(string msg)
         {
-            Paths.Create(_output);
-            using (var sw = File.AppendText(Path.Combine(_output, _logFile)))
+            _actionHelper.Safe(() =>
             {
-                try
+                Paths.Create(_output);
+                using (var sw = File.AppendText(Path.Combine(_output, _logFile)))
                 {
-                    var logLine = $"{DateTime.Now:G}: {msg}";
-                    sw.WriteLine(logLine);
+                    try
+                    {
+                        var logLine = $"{DateTime.Now:G}: {msg}";
+                        sw.WriteLine(logLine);
+                    }
+                    catch (Exception ex)
+                    {
+                        Exception(ex);
+                    }
+                    finally
+                    {
+                        sw.Close();
+                    }
                 }
-                catch (Exception ex)
-                {
-                    Exception(ex);
-                }
-                finally
-                {
-                    sw.Close();
-                }
-            }
+            });
+            
         }
 
         public void SetOutputFileName(string fileWithExtension)
@@ -60,18 +67,20 @@ namespace Ghpr.Core.Utils
 
         public void Exception(Exception exception, string exceptionMessage = "")
         {
-            var msg = (exceptionMessage.Equals("") ? "Exception!" : exceptionMessage) + Environment.NewLine
-                + " Message: " + Environment.NewLine + exception.Message + Environment.NewLine +
-                "StackTrace: " + Environment.NewLine + exception.StackTrace;
-            var inner = exception.InnerException;
-            while (inner != null)
-            {
-                msg = msg + Environment.NewLine + " Inner Exception: " + Environment.NewLine +
-                    inner.Message + Environment.NewLine +
-                "StackTrace: " + Environment.NewLine + inner.StackTrace;
-                inner = inner.InnerException;
-            }
-            WriteToFile(msg, "Exception_" + DateTime.Now.ToString("ddMMyy_HHmmss_fff") + ".txt");
+            _actionHelper.Safe(() => {
+                var msg = (exceptionMessage.Equals("") ? "Exception!" : exceptionMessage) + Environment.NewLine
+                    + " Message: " + Environment.NewLine + exception.Message + Environment.NewLine +
+                    "StackTrace: " + Environment.NewLine + exception.StackTrace;
+                var inner = exception.InnerException;
+                while (inner != null)
+                {
+                    msg = msg + Environment.NewLine + " Inner Exception: " + Environment.NewLine +
+                        inner.Message + Environment.NewLine +
+                    "StackTrace: " + Environment.NewLine + inner.StackTrace;
+                    inner = inner.InnerException;
+                }
+                WriteToFile(msg, "Exception_" + DateTime.Now.ToString("ddMMyy_HHmmss_fff") + ".txt");
+            });
         }
     }
 }
