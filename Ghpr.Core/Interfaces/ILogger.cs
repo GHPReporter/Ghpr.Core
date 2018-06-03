2df115e206e2a545ab470700f191fc5ceb06e448
Evgeniy Kosjakov <42_@mail.ru>
2018-06-03 23:36:07 +0300

    logging initial

---
+++ Ghpr.Core/Interfaces/ILogger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ghpr.Core.Interfaces
+{
+    public interface ILogger
+    {
+        void Info(Exception exception);
+        void Info(string message, Exception exception);
+        void Info(object message, Exception exception);
+
+        void Warn(Exception exception);
+        void Warn(string message, Exception exception);
+        void Warn(object message, Exception exception);
+
+        void Error(Exception exception);
+        void Error(string message, Exception exception);
+        void Error(object message, Exception exception);
+
+        void Debug(Exception exception);
+        void Debug(string message, Exception exception);
+        void Debug(object message, Exception exception);
+
+        void Fatal(Exception exception);
+        void Fatal(string message, Exception exception);
+        void Fatal(object message, Exception exception);
+
+        void Exception(Exception exception);
+        void Exception(string message, Exception exception);
+        void Exception(object message, Exception exception);
+    }
+}
---
+++ Ghpr.Logger/Logger.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Ghpr.Core.Interfaces;
+
+namespace Ghpr.Logger
+{
+    public class Logger : ILogger
+    {
+        public void Info(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Debug(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Debug(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Debug(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exception(Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exception(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exception(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
