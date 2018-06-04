2a63041553396210413440c1db7cded7fdf74c92
Evgeniy Kosjakov <42_@mail.ru>
2018-06-04 23:57:27 +0300

    Logger refactoring

--- Ghpr.Core/Interfaces/ILogger.cs
+++ Ghpr.Core/Interfaces/ILogger.cs
@@ -4,27 +4,27 @@ namespace Ghpr.Core.Interfaces
 {
     public interface ILogger
     {
-        void Info(Exception exception);
+        void Info(string message);
         void Info(string message, Exception exception);
         void Info(object message, Exception exception);
 
-        void Warn(Exception exception);
+        void Warn(string message);
         void Warn(string message, Exception exception);
         void Warn(object message, Exception exception);
 
-        void Error(Exception exception);
+        void Error(string message);
         void Error(string message, Exception exception);
         void Error(object message, Exception exception);
 
-        void Debug(Exception exception);
+        void Debug(string message);
         void Debug(string message, Exception exception);
         void Debug(object message, Exception exception);
 
-        void Fatal(Exception exception);
+        void Fatal(string message);
         void Fatal(string message, Exception exception);
         void Fatal(object message, Exception exception);
 
-        void Exception(Exception exception);
+        void Exception(string message);
         void Exception(string message, Exception exception);
         void Exception(object message, Exception exception);
     }
--- Ghpr.Logger/Logger.cs
+++ Ghpr.Logger/Logger.cs
@@ -1,98 +1,149 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using Ghpr.Core.Extensions;
 using Ghpr.Core.Interfaces;
 
 namespace Ghpr.Logger
 {
     public class Logger : ILogger
     {
-        public void Info(Exception exception)
+        private readonly string _logFileName;
+        private readonly string _outputPath;
+        private static readonly ReaderWriterLock Locker = new ReaderWriterLock();
+
+        private const string InfoLevel = "INFO";
+        private const string WarningLevel = "WARN";
+        private const string DebugLevel = "DEBUG";
+        private const string FatalLevel = "FATAL";
+        private const string ErrorLevel = "ERROR";
+        private const string ExceptionLevel = "EXCEPTION";
+
+        public Logger(string outputPath, string logFile)
         {
-            throw new NotImplementedException();
+            _outputPath = outputPath;
+            _logFileName = logFile;
+        }
+
+        private void Write(string msg, string logLevel)
+        {
+            try
+            {
+                Locker.AcquireWriterLock(int.MaxValue);
+                _outputPath.Create();
+                using (var sw = File.AppendText(Path.Combine(_outputPath, _logFileName)))
+                {
+                    var logLine = $"{DateTime.Now:yyyy.MM.dd-HH:mm:ss.ffffff} {logLevel}: {msg}";
+                    sw.WriteLine(logLine);
+                    sw.Close();
+                }
+            }
+            finally
+            {
+                Locker.ReleaseWriterLock();
+            }
+        }
+
+        private void WriteWithException(string message, Exception exception, string logLevel)
+        {
+            Write($"Message: {message}{Environment.NewLine}Exception: {exception.Message}{Environment.NewLine}" +
+                  $"Stack trace: {exception.StackTrace}", logLevel);
+        }
+
+        private void WriteWithException(object message, Exception exception, string logLevel)
+        {
+            Write($"Message: {message}{Environment.NewLine}Exception: {exception.Message}{Environment.NewLine}" +
+                  $"Stack trace: {exception.StackTrace}", logLevel);
+        }
+
+        public void Info(string message)
+        {
+            Write(message, InfoLevel);
         }
 
         public void Info(string message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, InfoLevel);
         }
 
         public void Info(object message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, InfoLevel);
         }
 
-        public void Warn(Exception exception)
+        public void Warn(string message)
         {
-            throw new NotImplementedException();
+            Write(message, WarningLevel);
         }
 
         public void Warn(string message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, WarningLevel);
         }
 
         public void Warn(object message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, WarningLevel);
         }
 
-        public void Error(Exception exception)
+        public void Error(string message)
         {
-            throw new NotImplementedException();
+            Write(message, ErrorLevel);
         }
 
         public void Error(string message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, ErrorLevel);
         }
 
         public void Error(object message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, ErrorLevel);
         }
 
-        public void Debug(Exception exception)
+        public void Debug(string message)
         {
-            throw new NotImplementedException();
+            Write(message, DebugLevel);
         }
 
         public void Debug(string message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, DebugLevel);
         }
 
         public void Debug(object message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, DebugLevel);
         }
 
-        public void Fatal(Exception exception)
+        public void Fatal(string message)
         {
-            throw new NotImplementedException();
+            Write(message, FatalLevel);
         }
 
         public void Fatal(string message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, FatalLevel);
         }
 
         public void Fatal(object message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, FatalLevel);
         }
 
-        public void Exception(Exception exception)
+        public void Exception(string message)
         {
-            throw new NotImplementedException();
+            Write(message, ExceptionLevel);
         }
 
         public void Exception(string message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, ExceptionLevel);
         }
 
         public void Exception(object message, Exception exception)
         {
-            throw new NotImplementedException();
+            WriteWithException(message, exception, ExceptionLevel);
         }
     }
 }
