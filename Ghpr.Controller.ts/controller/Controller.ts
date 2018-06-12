3720bf6b234eea73abb9c107463d1b38504d7f8a
Evgeniy Kosjakov <42_@mail.ru>
2018-06-12 22:31:51 +0300

    controller added

---
+++ Ghpr.Controller.ts/controller/Controller.ts
@@ -0,0 +1,30 @@
+﻿/// <reference path="./common/localFileSystem/LocalFileSystemDataService.ts" />
+
+class Controller {
+    static reportSettings: ReportSettingsDto;
+    static dataService: IDataService;
+
+    static init(settingsPath: string,
+        callback: (reportSettings: ReportSettingsDto, dataService: IDataService) => void): void {
+        const req = new XMLHttpRequest();
+        req.overrideMimeType("application/json");
+        req.open("get", settingsPath, true);
+        req.onreadystatechange = () => {
+            if (req.readyState === 4)
+                if (req.status !== 200 && req.status !== 0) {
+                    console
+                        .log(`Error while loading .json data: '${settingsPath}'! Status: ${req.status} : ${req.statusText}`);
+                } else {
+                    this.reportSettings = JSON.parse(req.responseText);
+                    this.dataService = new LocalFileSystemDataService();
+                    callback(this.reportSettings, this.dataService);
+                }
+        };
+        req.timeout = 2000;
+        req.ontimeout = () => {
+            console.log(
+                `Timeout while loading .json data: '${settingsPath}'! Status: ${req.status} : ${req.statusText}`);
+        };
+        req.send(null);
+    }
+}
--- Ghpr.Controller.ts/report/src/js/ghpr.controller.js
+++ Ghpr.Controller.ts/report/src/js/ghpr.controller.js
@@ -1662,4 +1662,28 @@ class Sorter {
         return 0;
     }
 }
+class Controller {
+    static init(settingsPath, callback) {
+        const req = new XMLHttpRequest();
+        req.overrideMimeType("application/json");
+        req.open("get", settingsPath, true);
+        req.onreadystatechange = () => {
+            if (req.readyState === 4)
+                if (req.status !== 200 && req.status !== 0) {
+                    console
+                        .log(`Error while loading .json data: '${settingsPath}'! Status: ${req.status} : ${req.statusText}`);
+                }
+                else {
+                    this.reportSettings = JSON.parse(req.responseText);
+                    this.dataService = new LocalFileSystemDataService();
+                    callback(this.reportSettings, this.dataService);
+                }
+        };
+        req.timeout = 2000;
+        req.ontimeout = () => {
+            console.log(`Timeout while loading .json data: '${settingsPath}'! Status: ${req.status} : ${req.statusText}`);
+        };
+        req.send(null);
+    }
+}
 //# sourceMappingURL=ghpr.controller.js.map
