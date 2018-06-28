70989ead9cb8800495f9b759380b6c70cf914412
Evgeniy Kosjakov <42_@mail.ru>
2018-06-28 13:17:29 +0300

    nuspecs added

--- Ghpr.Logger/Properties/AssemblyInfo.cs
+++ Ghpr.Logger/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Ghpr.Logger")]
+[assembly: AssemblyTitle("Ghpr.SimpleFileLogger")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Evgeniy Kosjakov")]
-[assembly: AssemblyProduct("Ghpr.Logger")]
+[assembly: AssemblyProduct("Ghpr.SimpleFileLogger")]
 [assembly: AssemblyCopyright("Copyright © Evgeniy Kosjakov 2016-2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.2.0")]
-[assembly: AssemblyFileVersion("0.8.2.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
