d03eac5956e3cb2a7cc17e163c3ac0ac9c0d0b5d
Evgeniy Kosjakov <42_@mail.ru>
2018-10-29 14:48:08 +0300

    references added

--- Ghpr.Core.Tests/UnitTest1.cs
+++ Ghpr.Core.Tests/UnitTest1.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Ghpr.Core.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void TestMethod()
         {
+
         }
     }
 }
