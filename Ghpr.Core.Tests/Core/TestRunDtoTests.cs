7abb8b516a2dfefbc10afa1941bd17f941804768
Evgeniy Kosjakov <42_@mail.ru>
2018-12-12 17:14:48 +0300

    Test run dto tests added

---
+++ Ghpr.Core.Tests/Core/TestRunDtoTests.cs
@@ -0,0 +1,40 @@
+﻿using Ghpr.Core.Common;
+using Ghpr.Core.Enums;
+using NUnit.Framework;
+
+namespace Ghpr.Core.Tests.Core
+{
+    [TestFixture]
+    public class TestRunDtoTests
+    {
+        [TestCase("passed", TestResult.Passed)]
+        [TestCase("Passed", TestResult.Passed)]
+        [TestCase("Passed test", TestResult.Passed)]
+        [TestCase("test passed", TestResult.Passed)]
+        [TestCase("error", TestResult.Broken)]
+        [TestCase("ERROR", TestResult.Broken)]
+        [TestCase("broken", TestResult.Broken)]
+        [TestCase("Broken", TestResult.Broken)]
+        [TestCase("failed", TestResult.Failed)]
+        [TestCase("failure", TestResult.Failed)]
+        [TestCase("Failed", TestResult.Failed)]
+        [TestCase("FAILURE", TestResult.Failed)]
+        [TestCase("inconclusive", TestResult.Inconclusive)]
+        [TestCase("Inconclusive", TestResult.Inconclusive)]
+        [TestCase("ignored", TestResult.Ignored)]
+        [TestCase("skipped", TestResult.Ignored)]
+        [TestCase("Ignored", TestResult.Ignored)]
+        [TestCase("notexecuted", TestResult.Ignored)]
+        [TestCase("as1dfd2fa4sd", TestResult.Unknown)]
+        [TestCase("123", TestResult.Unknown)]
+        [TestCase("!@#$%^&*(", TestResult.Unknown)]
+        public void TestResultTest(string testResult, TestResult result)
+        {
+            var test = new TestRunDto
+            {
+                Result = testResult
+            };
+            Assert.AreEqual(result, test.TestResult);
+        }
+    }
+}
