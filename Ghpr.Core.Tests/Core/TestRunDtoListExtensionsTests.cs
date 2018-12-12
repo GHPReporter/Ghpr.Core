7dde82668394b2d8354f5f1cc979728ee5828d04
Evgeniy Kosjakov <42_@mail.ru>
2018-12-12 19:33:43 +0300

    TestRunDtoListExtensionsTests

---
+++ Ghpr.Core.Tests/Core/TestRunDtoListExtensionsTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Ghpr.Core.Common;
+using Ghpr.Core.Extensions;
+using NUnit.Framework;
+
+namespace Ghpr.Core.Tests.Core
+{
+    [TestFixture]
+    public class TestRunDtoListExtensionsTests
+    {
+        [Test]
+        public void TestParallelDates()
+        {
+            var now = DateTime.Now;
+            var list = new List<KeyValuePair<TestRunDto, TestOutputDto>>
+            {
+                new KeyValuePair<TestRunDto, TestOutputDto>(new TestRunDto
+                {
+                    TestInfo = new ItemInfoDto { Start = now.AddSeconds(0), Finish = now.AddSeconds(5) }
+                }, new TestOutputDto()),
+                new KeyValuePair<TestRunDto, TestOutputDto>(new TestRunDto
+                {
+                    TestInfo = new ItemInfoDto { Start = now.AddSeconds(2), Finish = now.AddSeconds(3) }
+                }, new TestOutputDto())
+            };
+            var start = list.GetRunStartDateTime();
+            var finish = list.GetRunFinishDateTime();
+            Assert.AreEqual(now.AddSeconds(0), start);
+            Assert.AreEqual(now.AddSeconds(5), finish);
+        }
+
+        [Test]
+        public void TestFilledDates()
+        {
+            var now = DateTime.Now;
+            var list = new List<KeyValuePair<TestRunDto, TestOutputDto>>
+            {
+                new KeyValuePair<TestRunDto, TestOutputDto>(new TestRunDto
+                {
+                    TestInfo = new ItemInfoDto { Start = now.AddSeconds(0), Finish = now.AddSeconds(1) }
+                }, new TestOutputDto()),
+                new KeyValuePair<TestRunDto, TestOutputDto>(new TestRunDto
+                {
+                    TestInfo = new ItemInfoDto { Start = now.AddSeconds(2), Finish = now.AddSeconds(3) }
+                }, new TestOutputDto())
+            };
+            var start = list.GetRunStartDateTime();
+            var finish = list.GetRunFinishDateTime();
+            Assert.AreEqual(now.AddSeconds(0), start);
+            Assert.AreEqual(now.AddSeconds(3), finish);
+        }
+    }
+}
