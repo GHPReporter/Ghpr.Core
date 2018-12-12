790ea237c040495bad7ec76d87efdebb112652c3
Evgeniy Kosjakov <42_@mail.ru>
2018-12-12 19:55:36 +0300

    Some SimpleItemInfoDtoComparer Tests

---
+++ Ghpr.Core.Tests/Core/SimpleItemInfoDtoComparerTests.cs
@@ -0,0 +1,28 @@
+﻿using Ghpr.Core.Common;
+using Ghpr.Core.Comparers;
+using NUnit.Framework;
+
+namespace Ghpr.Core.Tests.Core
+{
+    [TestFixture]
+    public class SimpleItemInfoDtoComparerTests
+    {
+        [Test]
+        public void TestEmptyItems()
+        {
+            var dto1 = new SimpleItemInfoDto();
+            var dto2 = new SimpleItemInfoDto();
+            var comparer = new SimpleItemInfoDtoComparer();
+            var res = comparer.Equals(dto1, dto2);
+            Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void TestHash()
+        {
+            var dto = new SimpleItemInfoDto();
+            var comparer = new SimpleItemInfoDtoComparer();
+            Assert.AreEqual(dto.GetHashCode(), comparer.GetHashCode(dto));
+        }
+    }
+}
