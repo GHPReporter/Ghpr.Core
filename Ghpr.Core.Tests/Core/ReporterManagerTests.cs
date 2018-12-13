f580cc97606996e1e0dbec16a0ccbe568827e59c
Evgeniy Kosjakov <42_@mail.ru>
2018-12-13 13:59:02 +0300

    reporter and reporter manager tests

---
+++ Ghpr.Core.Tests/Core/ReporterManagerTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Ghpr.Core.Enums;
+using Ghpr.Core.Settings;
+using NUnit.Framework;
+
+namespace Ghpr.Core.Tests.Core
+{
+    [TestFixture]
+    public class ReporterManagerTests
+    {
+        [Test]
+        public void CreationTest()
+        {
+            ReporterManager.Initialize(new MockTestDataProvider());
+            Assert.AreEqual("C:\\_GHPReporter_Core_Report", ReporterManager.OutputPath);
+        }
+
+        [TestCase(TestingFramework.NUnit, "C:\\_GHPReporter_NUnit_Report")]
+        [TestCase(TestingFramework.MSTest, "C:\\_GHPReporter_MSTest_Report")]
+        [TestCase(TestingFramework.MSTestV2, "C:\\_GHPReporter_MSTestV2_Report")]
+        [TestCase(TestingFramework.SpecFlow, "C:\\_GHPReporter_SpecFlow_Report")]
+        public void CanCreateByFramework(TestingFramework framework, string outputPath)
+        {
+            ReporterManager.Initialize(framework, new MockTestDataProvider());
+            Assert.AreEqual(outputPath, ReporterManager.OutputPath);
+        }
+
+        [Test]
+        public void CanCreateWithSettings()
+        {
+            var s = new ReporterSettings
+            {
+                RunGuid = Guid.NewGuid().ToString(),
+                DataServiceFile = "Ghpr.LocalFileSystem.dll",
+                LoggerFile = "",
+                OutputPath = @"\\server\folder",
+                ProjectName = "cool project",
+                RealTimeGeneration = true,
+                ReportName = "report name",
+                Retention = new RetentionSettings
+                {
+                    Amount = 3,
+                    Till = DateTime.Now
+                },
+                RunName = "run name",
+                RunsToDisplay = 7
+            };
+            ReporterManager.Initialize(s, new MockTestDataProvider());
+            Assert.AreEqual(s.OutputPath, ReporterManager.OutputPath);
+        }
+    }
+}
---
+++ Ghpr.Core.Tests/Core/ReporterTests.cs
@@ -0,0 +1,38 @@
+﻿using Ghpr.Core.Common;
+using Ghpr.Core.Factories;
+using NUnit.Framework;
+
+namespace Ghpr.Core.Tests.Core
+{
+    [TestFixture]
+    public class ReporterTests
+    {
+        [Test]
+        public void TestReportProcess()
+        {
+            var r = ReporterFactory.Build(new MockTestDataProvider());
+            Assert.IsFalse(r.TestRunStarted);
+            r.RunStarted();
+            Assert.IsTrue(r.TestRunStarted);
+            Assert.AreEqual(0, r.RunRepository.CurrentRun.TestsInfo.Count);
+            Assert.AreEqual(0, r.RunRepository.CurrentRun.RunSummary.Total);
+            Assert.AreEqual("", r.RunRepository.CurrentRun.Name);
+            r.SetRunName("new name");
+            Assert.AreEqual("new name", r.RunRepository.CurrentRun.Name);
+            r.TestStarted(new TestRunDto());
+            r.TestFinished(new TestRunDto(), new TestOutputDto());
+            r.AddCompleteTestRun(new TestRunDto(), new TestOutputDto());
+            r.RunFinished();
+            r.TearDown();
+        }
+
+        [Test]
+        public void TestSetTestDataProvider()
+        {
+            var r = ReporterFactory.Build(new MockTestDataProvider());
+            Assert.IsInstanceOf(typeof(MockTestDataProvider), r.TestDataProvider);
+            r.SetTestDataProvider(new MockTestDataProviderWithException());
+            Assert.IsInstanceOf(typeof(MockTestDataProviderWithException), r.TestDataProvider);
+        }
+    }
+}
