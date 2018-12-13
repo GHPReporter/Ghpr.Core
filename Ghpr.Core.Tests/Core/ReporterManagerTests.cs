7a3d88fdb93a42cb878cda51ed6ca1cc7b8dd140
Evgeniy Kosjakov <42_@mail.ru>
2018-12-13 14:27:37 +0300

    more manager tests

--- Ghpr.Core.Tests/Core/ReporterManagerTests.cs
+++ Ghpr.Core.Tests/Core/ReporterManagerTests.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
+using Ghpr.Core.Common;
 using Ghpr.Core.Enums;
+using Ghpr.Core.Interfaces;
 using Ghpr.Core.Settings;
 using NUnit.Framework;
 
@@ -11,6 +15,7 @@ namespace Ghpr.Core.Tests.Core
         [Test]
         public void CreationTest()
         {
+            ReporterManager.TearDown();
             ReporterManager.Initialize(new MockTestDataProvider());
             Assert.AreEqual("C:\\_GHPReporter_Core_Report", ReporterManager.OutputPath);
         }
@@ -21,6 +26,7 @@ namespace Ghpr.Core.Tests.Core
         [TestCase(TestingFramework.SpecFlow, "C:\\_GHPReporter_SpecFlow_Report")]
         public void CanCreateByFramework(TestingFramework framework, string outputPath)
         {
+            ReporterManager.TearDown();
             ReporterManager.Initialize(framework, new MockTestDataProvider());
             Assert.AreEqual(outputPath, ReporterManager.OutputPath);
         }
@@ -28,6 +34,7 @@ namespace Ghpr.Core.Tests.Core
         [Test]
         public void CanCreateWithSettings()
         {
+            ReporterManager.TearDown();
             var s = new ReporterSettings
             {
                 RunGuid = Guid.NewGuid().ToString(),
@@ -47,6 +54,34 @@ namespace Ghpr.Core.Tests.Core
             };
             ReporterManager.Initialize(s, new MockTestDataProvider());
             Assert.AreEqual(s.OutputPath, ReporterManager.OutputPath);
+        }
+
+        [Test]
+        public void CanSetTestDataProvider()
+        {
+            ReporterManager.TearDown();
+            ReporterManager.Initialize(new MockTestDataProvider());
+            Assert.DoesNotThrow(() => ReporterManager.SetTestDataProvider(new MockTestDataProviderWithException()));
+        }
+
+        [Test]
+        public void Process()
+        {
+            ReporterManager.TearDown();
+            ReporterManager.Initialize(new MockTestDataProvider());
+            ReporterManager.RunStarted();
+            ReporterManager.TestStarted(new TestRunDto());
+            ReporterManager.TestFinished(new TestRunDto(), new TestOutputDto
+            {
+                Output = "test output",
+                SuiteOutput = "suite output",
+                TestOutputInfo = new SimpleItemInfoDto
+                    {
+                        Date = DateTime.Now,
+                        ItemName = "item name"
+                    }
+            });
+            ReporterManager.RunFinished();
         }
     }
 }
