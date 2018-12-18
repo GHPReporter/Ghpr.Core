97e33fa0f884e81a448773e3ef70f38fda4f6536
Evgeniy Kosjakov <42_@mail.ru>
2018-12-18 16:55:54 +0300

    local file system to v0.9.4

--- Ghpr.LocalFileSystem/Properties/AssemblyInfo.cs
+++ Ghpr.LocalFileSystem/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Evgeniy Kosjakov")]
 [assembly: AssemblyProduct("Ghpr.LocalFileSystem")]
-[assembly: AssemblyCopyright("Copyright © Evgeniy Kosjakov 2016-2018")]
+[assembly: AssemblyCopyright("Copyright © Evgeniy Kosjakov 2016-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.2.0")]
-[assembly: AssemblyFileVersion("0.9.2.0")]
+[assembly: AssemblyVersion("0.9.4.0")]
+[assembly: AssemblyFileVersion("0.9.4.0")]
