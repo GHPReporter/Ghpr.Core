dc1024bf9e7da94b17083fa73cad47204b588351
Evgeniy Kosjakov <42_@mail.ru>
2019-06-23 17:16:32 +0300

    Update Tests.ps1

--- Tests.ps1
+++ Tests.ps1
@@ -12,7 +12,12 @@ write-host "======= DESTINATION PATH: " $destinationRoot " ======="
 
 Copy-Item -Path $sourceRoot -Filter "*.*" -Recurse -Destination $destinationRoot -Container -Verbose
 
-$nunit = $destinationRoot + "\nunit3-console.exe"
+$nunit = $destinationRoot + "nunit3-console.exe"
 write-host "======= NUNIT PATH: " $nunit " ======="
+
+$sourceRoot1 = "$($env:USERPROFILE)\.nuget\packages\NUnit\3.12.0\lib\netstandard2.0\*"
+write-host "======= SOURCE PATH: " $sourceRoot1 " ======="
+
+Copy-Item -Path $sourceRoot1 -Filter "*.*" -Recurse -Destination $destinationRoot -Container -Verbose
 
 & $opencover -register:user -target:$nunit "-targetargs:""Ghpr.Tests.Tests\bin\Release\netcoreapp2.1\Ghpr.Core.Tests.dll""" -filter:"+[Ghpr.Core*]* -[Ghpr.Tests.Tests*]*" -output:opencoverCoverage.xml
