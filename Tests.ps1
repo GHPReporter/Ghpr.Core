a9c8527a4755b139d240aacbdb1b2af189a3b27e
Evgeniy Kosjakov <42_@mail.ru>
2019-06-23 17:26:05 +0300

    Update Tests.ps1

--- Tests.ps1
+++ Tests.ps1
@@ -1,23 +1,4 @@
 $opencover = "$($env:USERPROFILE)\.nuget\packages\opencover\4.7.922\tools\OpenCover.Console.exe"
 write-host "======= OPENCOVER PATH: " $opencover " ======="
 
-$build = "$($env:APPVEYOR_BUILD_FOLDER)"
-write-host "======= BUILD PATH: " $build " ======="
-
-$sourceRoot = "$($env:USERPROFILE)\.nuget\packages\nunit.consolerunner\3.10.0\tools\*"
-write-host "======= SOURCE PATH: " $sourceRoot " ======="
-
-$destinationRoot = $build + "\Ghpr.Tests.Tests\bin\Release\netcoreapp2.1\"
-write-host "======= DESTINATION PATH: " $destinationRoot " ======="
-
-Copy-Item -Path $sourceRoot -Filter "*.*" -Recurse -Destination $destinationRoot -Container -Verbose
-
-$nunit = $destinationRoot + "nunit3-console.exe"
-write-host "======= NUNIT PATH: " $nunit " ======="
-
-$sourceRoot1 = "$($env:USERPROFILE)\.nuget\packages\NUnit\3.12.0\lib\netstandard2.0\*"
-write-host "======= SOURCE PATH: " $sourceRoot1 " ======="
-
-Copy-Item -Path $sourceRoot1 -Filter "*.*" -Recurse -Destination $destinationRoot -Container -Verbose
-
-& $opencover -register:user -target:$nunit "-targetargs:""Ghpr.Tests.Tests\bin\Release\netcoreapp2.1\Ghpr.Core.Tests.dll""" -filter:"+[Ghpr.Core*]* -[Ghpr.Tests.Tests*]*" -output:opencoverCoverage.xml
+& $opencover -register:user -target:"dotnet.exe" "-targetargs:""Ghpr.Tests.Tests\bin\Release\netcoreapp2.1\Ghpr.Core.Tests.*""" -filter:"+[Ghpr.Core*]* -[Ghpr.Tests.Tests*]*" -output:opencoverCoverage.xml
