76ea6fe7915cf08ebd416d441ac944eb31de3dc3
Evgeniy Kosjakov <42_@mail.ru>
2019-06-23 14:02:35 +0300

    test script

--- Tests.ps1
+++ Tests.ps1
@@ -1,1 +1,5 @@
-& .\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:"nunit3-console.exe" "-targetargs:""Ghpr.Core.Tests\bin\Release\Ghpr.Core.Tests.dll""" -filter:"+[Ghpr.Core*]* -[Ghpr.Core.Tests*]*" -output:opencoverCoverage.xml
+$opencover = "$($env:USERPROFILE)\.nuget\packages\OpenCover.4.7.922\tools\OpenCover.Console.exe"
+
+write-host "======= OPENCOVER PATH: " $opencover " ======="
+
+$opencover -register:user -target:"nunit3-console.exe" "-targetargs:""Ghpr.Tests.Tests\bin\Release\netcoreapp2.1\Ghpr.Core.Tests.dll""" -filter:"+[Ghpr.Core*]* -[Ghpr.Tests.Tests*]*" -output:opencoverCoverage.xml
