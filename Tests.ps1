6ffff3d5eeb58d5e102f6deab8ffb7b62414c87c
Evgeniy Kosjakov <42_@mail.ru>
2019-06-23 17:30:49 +0300

    Update Tests.ps1

--- Tests.ps1
+++ Tests.ps1
@@ -1,4 +1,4 @@
 $opencover = "$($env:USERPROFILE)\.nuget\packages\opencover\4.7.922\tools\OpenCover.Console.exe"
 write-host "======= OPENCOVER PATH: " $opencover " ======="
 
-& $opencover -register:user -target:"dotnet.exe" "-targetargs:""Ghpr.Tests.Tests\Ghpr.Core.Tests.csproj""" -filter:"+[Ghpr.Core*]* -[Ghpr.Tests.Tests*]*" -output:opencoverCoverage.xml
+& $opencover -register:user -target:"dotnet.exe" "-targetargs:""test Ghpr.Tests.Tests\Ghpr.Core.Tests.csproj""" -filter:"+[Ghpr.Core*]* -[Ghpr.Tests.Tests*]*" -output:opencoverCoverage.xml
