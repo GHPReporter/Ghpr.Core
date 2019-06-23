5544f06c9067a766080d4a47e3bd44c08033c0ea
Evgeniy Kosjakov <42_@mail.ru>
2019-06-23 19:22:07 +0300

    coverage uploading fixed

--- Coverage.ps1
+++ Coverage.ps1
@@ -1,6 +1,6 @@
-$coveralls = (Resolve-Path "packages/coveralls.net.*/tools/csmacnz.coveralls.exe").ToString()
+dotnet tool install -g coveralls.net --version 1.0.0
 
-write-host "======= COVERALLS PATH: " $coveralls " ======="
+$coveralls = "csmacnz.coveralls"
 
 if ($env:APPVEYOR_PULL_REQUEST_NUMBER -eq $null) {
 	& $coveralls --opencover -i opencoverCoverage.xml --repoToken $env:COVERALLS_REPO_TOKEN --commitId $env:APPVEYOR_REPO_COMMIT --commitBranch $env:APPVEYOR_REPO_BRANCH --commitAuthor $env:APPVEYOR_REPO_COMMIT_AUTHOR --commitEmail $env:APPVEYOR_REPO_COMMIT_AUTHOR_EMAIL --commitMessage $env:APPVEYOR_REPO_COMMIT_MESSAGE --jobId $env:APPVEYOR_JOB_ID
@@ -12,9 +12,9 @@ else {
 
 $result = $LASTEXITCODE
 
-$codecov = (Resolve-Path "packages/Codecov.*/tools/codecov.exe").ToString()
-
+$codecov = "$($env:USERPROFILE)\.nuget\packages\codecov\1.5.0\tools\codecov.exe"
 write-host "======= CODECOV PATH: " $codecov " ======="
+
 & $codecov -f "opencoverCoverage.xml"
 
 if($result -ne 0){
