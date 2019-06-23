702c5867479a93c259d2b2074121cfb914dcfe13
Evgeniy Kosjakov <42_@mail.ru>
2019-06-23 14:51:24 +0300

    nunit console installed

--- Tests.ps1
+++ Tests.ps1
@@ -2,4 +2,8 @@ $opencover = "$($env:USERPROFILE)\.nuget\packages\opencover\4.7.922\tools\OpenCo
 
 write-host "======= OPENCOVER PATH: " $opencover " ======="
 
-& $opencover -register:user -target:"nunit3-console.exe" "-targetargs:""Ghpr.Tests.Tests\bin\Release\netcoreapp2.1\Ghpr.Core.Tests.dll""" -filter:"+[Ghpr.Core*]* -[Ghpr.Tests.Tests*]*" -output:opencoverCoverage.xml
+$nunit = "$($env:USERPROFILE)\.nuget\packages\nunit.consolerunner\3.10.0\tools\nunit3-console.exe"
+
+write-host "======= NUNIT PATH: " $nunit " ======="
+
+& $opencover -register:user -target:$nunit "-targetargs:""Ghpr.Tests.Tests\bin\Release\netcoreapp2.1\Ghpr.Core.Tests.dll""" -filter:"+[Ghpr.Core*]* -[Ghpr.Tests.Tests*]*" -output:opencoverCoverage.xml
