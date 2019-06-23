41df8006a2efb4cff134f6ad7c930c9d29b4a007
Evgeniy Kosjakov <42_@mail.ru>
2019-06-23 17:01:00 +0300

    Update Tests.ps1

--- Tests.ps1
+++ Tests.ps1
@@ -1,18 +1,18 @@
 $opencover = "$($env:USERPROFILE)\.nuget\packages\opencover\4.7.922\tools\OpenCover.Console.exe"
-
 write-host "======= OPENCOVER PATH: " $opencover " ======="
 
-$nunit = "$($env:USERPROFILE)\.nuget\packages\nunit.consolerunner\3.10.0\tools\nunit3-console.exe"
-
-write-host "======= NUNIT PATH: " $nunit " ======="
-
 $build = "$($env:APPVEYOR_BUILD_FOLDER)"
-
 write-host "======= BUILD PATH: " $build " ======="
 
 $sourceRoot = "$($env:USERPROFILE)\.nuget\packages\nunit.consolerunner\3.10.0\tools"
+write-host "======= SOURCE PATH: " $sourceRoot " ======="
+
 $destinationRoot = $build + "\Ghpr.Tests.Tests\bin\Release\netcoreapp2.1\"
+write-host "======= DESTINATION PATH: " $destinationRoot " ======="
 
-Copy-Item -Path $sourceRoot -Filter "*.*" -Recurse -Destination $destinationRoot -Container
+Copy-Item -Path $sourceRoot -Filter "*.*" -Recurse -Destination $destinationRoot -Container -Verbose
 
-& $opencover -register:user -target:"nunit3-console.exe" "-targetargs:""Ghpr.Tests.Tests\bin\Release\netcoreapp2.1\Ghpr.Core.Tests.dll""" -filter:"+[Ghpr.Core*]* -[Ghpr.Tests.Tests*]*" -output:opencoverCoverage.xml
+$nunit = $destinationRoot + "\nunit3-console.exe"
+write-host "======= NUNIT PATH: " $nunit " ======="
+
+& $opencover -register:user -target:$nunit "-targetargs:""Ghpr.Tests.Tests\bin\Release\netcoreapp2.1\Ghpr.Core.Tests.dll""" -filter:"+[Ghpr.Core*]* -[Ghpr.Tests.Tests*]*" -output:opencoverCoverage.xml
